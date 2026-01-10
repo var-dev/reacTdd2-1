9532a4c36968ddc152eef5f6859012bc78b22983
user <user@localhost>
2026-01-09 21:25:41 -0700

    parameterized tests

--- src/customerForm.tsx
+++ src/customerForm.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import type { Customer } from "./appointmentsDayView.js"
 export type CustomerFormProps = {
   customer?: Customer
@@ -8,13 +8,20 @@ export const CustomerForm = ({customer, onSubmit}: CustomerFormProps) =>{
   function onSubmitHandler(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault() 
     if (onSubmit) {
-      return onSubmit(customer!)
+      return onSubmit(customerState)
     }
   }
+  const handleChangeFirstName = ({ target }: React.ChangeEvent<HTMLInputElement>) =>
+    setCustomerState((customerState) => ({
+      ...customerState,
+      firstName: target.value
+    }));
+  const [customerState, setCustomerState] = useState<Customer>(customer ?? {firstName: ""});
+
   
   return <form onSubmit = {onSubmitHandler} aria-label="Customer form">
     <label htmlFor="firstName">First Name</label>
-    <input type="text" name="firstName" id="firstName" defaultValue={customer?.firstName} />
+    <input type="text" name="firstName" id="firstName" value={customerState.firstName} onChange={handleChangeFirstName}/>
     <input type="submit" value="Add" />
   </form>
 }
--- test/customerForm2.test.tsx
+++ test/customerForm2.test.tsx
@@ -53,3 +53,79 @@ describe('CustomerForm tests using render and screen', ()=>{
       assert.strictEqual(mockOnSubmitHandler.mock.calls.length, 1, `mockOnSubmitHandler was called ${mockOnSubmitHandler.mock.calls.length} times instead of 1 time`);;
     })
 })
+
+
+const itRendersAsATextBox = (
+  label: string
+  ) =>it("renders as a text box with label", () => {
+    render(<CustomerForm customer={originalProps.customer}/>)
+    const textBoxInputElement = screen.getByLabelText(label) as HTMLInputElement;
+
+    assert.ok(textBoxInputElement instanceof HTMLInputElement, 'It should be instance of HTMLInputElement')
+  });
+
+const itIncludesTheExistingValue = (
+  label: string,
+  existing: string
+  ) => it("includes the existing value", () => {
+    render(<CustomerForm customer={originalProps.customer}/>)
+    const textBoxInputElement = screen.getByLabelText(label) as HTMLInputElement;
+
+    assert.strictEqual(textBoxInputElement.value, existing, `It should be ${existing}`)
+  });
+
+const itSavesExistingValueWhenSubmitted = (
+  key: keyof Customer,
+  value: string
+  ) => it("saves existing value when submitted", async () => {
+    const submitEvent = userEvent.setup();
+    const mockOnSubmitHandler = mock.fn((customer: Customer)=>{});
+    const mockEventListenerHandler = mock.fn((e: Event) => {})
+    render(
+      <CustomerForm 
+        customer={originalProps.customer}
+        onSubmit={mockOnSubmitHandler}
+      />
+    )
+    const form = screen.getByRole("form", { name: /customer/i });
+    form.addEventListener("submit", mockEventListenerHandler);
+    const submitButton = screen.getByRole('button', { name: /Add/i })
+    
+    await submitEvent.click(submitButton)
+
+    assert.strictEqual(mockEventListenerHandler.mock.calls.length, 1, `mockEventListenerHandler was called ${mockEventListenerHandler.mock.calls.length} times instead of 1 time`);;
+    assert.strictEqual(mockEventListenerHandler.mock.calls[0].arguments[0].type, "submit", 'Event is not of Submit type');
+    assert.strictEqual(mockEventListenerHandler.mock.calls[0].arguments[0].defaultPrevented, true, 'PreventDefault was not set correctly');
+    assert.strictEqual(mockOnSubmitHandler.mock.calls.length, 1, `mockOnSubmitHandler was called ${mockOnSubmitHandler.mock.calls.length} times instead of 1 time`);
+    assert.strictEqual(mockOnSubmitHandler.mock.calls[0].arguments[0][key], value, `Object[${key}] value is not ${value}`);
+  })
+
+const itSavesNewValueWhenSubmitted = (
+  label: string,
+  key: keyof Customer,
+  newValue: string
+  ) => it("saves new value when submitted", async () => {
+    const submitEvent = userEvent.setup();
+    const mockOnSubmitHandler = mock.fn((customer: Customer)=>{});
+    render(
+      <CustomerForm 
+        customer={originalProps.customer}
+        onSubmit={mockOnSubmitHandler}
+      />
+    )
+    const submitButton = screen.getByRole('button', { name: /Add/i })
+    const textBoxInputElement = screen.getByLabelText(label) as HTMLInputElement;
+    await userEvent.clear(textBoxInputElement);
+    await userEvent.type(textBoxInputElement, newValue)
+    await submitEvent.click(submitButton)
+
+    assert.strictEqual(mockOnSubmitHandler.mock.calls.length, 1, `mockOnSubmitHandler was called ${mockOnSubmitHandler.mock.calls.length} times instead of 1 time`);
+    assert.strictEqual(mockOnSubmitHandler.mock.calls[0].arguments[0][key], newValue, `Object[${key}] value is not ${newValue}`);
+  })
+
+describe("first name field", () => {
+  itRendersAsATextBox('First Name');
+  itIncludesTheExistingValue('First Name', 'Jane');
+  itSavesExistingValueWhenSubmitted('firstName', 'Jane');
+  itSavesNewValueWhenSubmitted('First Name', 'firstName', 'Jamie');
+});
