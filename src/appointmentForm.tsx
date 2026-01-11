3ab37e76bb27c4215bbcbf10e335c7779b86694c
user <user@localhost>
2026-01-10 22:39:03 -0700

    saving changes before adding appointment table

--- src/appointmentForm.tsx
+++ src/appointmentForm.tsx
@@ -1,22 +1,35 @@
 import React from "react"
 
-export const AppointmentForm = ({
-  selectableServices
-}: any) =>{
-  return <form aria-label="Appointment form">
-    <label htmlFor="service">Service</label>
-    <select name="service" id="service">
-      {selectableServices.map((service: string)=><option key={service}>{service}</option>)}
-    </select>
-  </form>
-}
-AppointmentForm.defaultProps = {
-  selectableServices: [
+const selectableServices = [
     "Cut",
     "Blow-dry",
     "Cut & color",
     "Beard trim",
     "Cut & beard trim",
     "Extensions",
-  ]
+  ] as const
+export type Service = typeof selectableServices[number]
+export type Appointment = {
+  service: Service
+}
+
+  type AppointmentFormType = {
+    selectableServices: Service[],
+    appointment: Appointment
+  }
+
+export const AppointmentForm = ({
+  selectableServices,
+  appointment
+}: AppointmentFormType) =>{
+  return <form aria-label="Appointment form">
+    <label htmlFor="service">Service</label>
+    <select name="service" id="service" value={appointment.service} onChange={()=>{}}>
+      {selectableServices.map((service: string)=><option key={service}>{service}</option>)}
+    </select>
+  </form>
+}
+AppointmentForm.defaultProps = {
+  selectableServices
 };
+
--- test/appointmentForm.test.tsx
+++ test/appointmentForm.test.tsx
@@ -3,11 +3,11 @@ import * as assert from 'node:assert/strict';
 
 import "./domSetup"; // must be imported before render/screen
 import * as React from "react";
-import { act } from "react";
 
 import { render, screen, cleanup } from "@testing-library/react";
 
 import { AppointmentForm } from "../src/appointmentForm";
+import type { Service, Appointment } from "../src/appointmentForm";
 
 beforeEach( () => {
   globalThis.document.body.innerHTML = '<p>Hello world</p>';
@@ -17,40 +17,57 @@ afterEach(()=>{
   globalThis.document.body.innerHTML = ' ';
 })
 
-function labelsOfAllOptions(element: HTMLSelectElement) {
+function labelsOfAllOptions(selectBox: HTMLSelectElement) {
   return Array.from(
-    element.childNodes,
+    selectBox.childNodes,
     (node) => node.textContent
   );
 }
+function findOption(selectBox: HTMLSelectElement, textContent: string) {
+  const options = Array.from(selectBox.childNodes);
+  return options.find(
+    option => option.textContent === textContent
+  );
+};
+const blankAppointment: Appointment = {
+  service: "Cut",
+};
 
 describe('Appointment form', ()=>{
   it("renders appointment form", async () => {
-    render(<AppointmentForm />)
+    render(<AppointmentForm appointment={blankAppointment}/>)
     const form = screen.getByLabelText('Appointment form') as HTMLFormElement;
 
     assert.ok(form, 'No form found with aria-label "Appointment form"');
   })
   describe('service field', ()=>{
+    const services: Service[] = ["Cut", "Blow-dry" ];
     it('renders select box', ()=>{
-      render(<AppointmentForm />)
+      render(<AppointmentForm appointment={blankAppointment} />)
       const select = screen.getByLabelText('Service') as HTMLSelectElement;
 
       assert.ok(select, 'No select box found with aria-label "Service"');
       assert.strictEqual(select.tagName, 'SELECT', `Expected tag name select, but got ${select.tagName}`)
     })
-    it('has a blank item as a first value', ()=>{
-      render(<AppointmentForm selectableServices={['']}/>)
+    it('has a service item as a first value', ()=>{
+      render(<AppointmentForm selectableServices={['Cut']} appointment={blankAppointment}/>)
       const select = screen.getByRole('combobox', {name: 'Service'}) as HTMLSelectElement;
 
-      assert.strictEqual(labelsOfAllOptions(select)[0], '', `Expected empty node`)
+      assert.strictEqual(labelsOfAllOptions(select)[0], 'Cut', `Expected service Cut`)
     })
     it("lists all salon services", () => {
-      const services = ["Cut", "Blow-dry" ];
-      render(<AppointmentForm selectableServices={services}/>)
+      render(<AppointmentForm selectableServices={services} appointment={blankAppointment}/>)
       const select = screen.getByLabelText('Service') as HTMLSelectElement;
 
       assert.deepStrictEqual(labelsOfAllOptions(select), services, `Expected array ${JSON.stringify(services)}`)
+    })
+    it("pre selects value", () => {
+      const appointment: Appointment = { service: "Blow-dry" };
+      render(<AppointmentForm selectableServices={services} appointment={appointment}/>)
+      const select = screen.getByLabelText('Service') as HTMLSelectElement;
+      const option = findOption(select, appointment.service) as HTMLOptionElement
+
+      assert.ok(option.tagName==='OPTION' && option.selected, `Expected option ${appointment.service} to be selected`)
     })
   })
 })
