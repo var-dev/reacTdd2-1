a367bc7d167758c9a581baee38a09f01b7cdc8cc
user <user@localhost>
2026-01-11 22:51:01 -0700

    p120 table headings completed

--- src/appointmentForm.tsx
+++ src/appointmentForm.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 
+
 const selectableServices = [
     "Cut",
     "Blow-dry",
@@ -15,21 +16,89 @@ export type Appointment = {
 
   type AppointmentFormType = {
     selectableServices: Service[],
-    appointment: Appointment
+    salonOpensAt: number,
+    salonClosesAt: number,
+    appointment: Appointment,
+    today: Date
   }
 
 export const AppointmentForm = ({
   selectableServices,
-  appointment
+  salonOpensAt,
+  salonClosesAt,
+  appointment,
+  today
 }: AppointmentFormType) =>{
   return <form aria-label="Appointment form">
     <label htmlFor="service">Service</label>
     <select name="service" id="service" value={appointment.service} onChange={()=>{}}>
       {selectableServices.map((service: string)=><option key={service}>{service}</option>)}
     </select>
+    <TimeSlotTable salonOpensAt={salonOpensAt} salonClosesAt={salonClosesAt} today={today}/>
   </form>
 }
 AppointmentForm.defaultProps = {
-  selectableServices
+  selectableServices,
+  salonOpensAt: 9,
+  salonClosesAt: 19,
+  today: new Date(1970, 1, 1)
 };
 
+
+
+const timeIncrements = (
+  numberOfTimeSlots: number,
+  startTime: number,
+  increment: number
+) =>
+  Array(numberOfTimeSlots)
+    .fill([startTime])
+    .reduce((acc, _, i) =>
+      acc.concat([startTime + i * increment])
+    );
+
+const dailyTimeSlots = (
+  salonOpensAt: number,
+  salonClosesAt: number
+) => {
+  const totalSlots = (salonClosesAt - salonOpensAt) * 2;
+  const startTime = new Date().setHours(salonOpensAt, 0, 0, 0);
+  const increment = 30 * 60 * 1000;
+  return timeIncrements(
+    totalSlots,
+    startTime,
+    increment
+  );
+};
+
+const toTimeValue = (timestamp:number) => new Date(timestamp).toTimeString().substring(0, 5);
+
+const weeklyDateValues = (startDate: Date) => {
+  const midnight = startDate.setHours(0, 0, 0, 0);
+  const increment = 24 * 60 * 60 * 1000;
+  return timeIncrements(7, midnight, increment);
+};
+const toShortDate = (timestamp: number) => {
+  const [day, , dayOfMonth] = new Date(timestamp).toDateString().split(" ");
+  return `${day} ${dayOfMonth}`;
+};
+
+const TimeSlotTable = ({salonOpensAt, salonClosesAt, today}: {salonOpensAt: number, salonClosesAt: number, today: Date})=>{
+  const timeSlots = dailyTimeSlots(salonOpensAt, salonClosesAt);
+  const dates = weeklyDateValues(today);
+  return <table aria-label="time slot table" >
+    <thead>
+      <tr>
+        <th />
+        {dates.map((d: number) => (<th key={d}>{toShortDate(d)}</th>))}
+      </tr>
+    </thead>
+    <tbody>
+      {timeSlots.map((timeSlot: number) => (
+        <tr key={timeSlot}>
+          <th>{toTimeValue(timeSlot)}</th>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+}
--- test/appointmentForm.test.tsx
+++ test/appointmentForm.test.tsx
@@ -4,7 +4,7 @@ import * as assert from 'node:assert/strict';
 import "./domSetup"; // must be imported before render/screen
 import * as React from "react";
 
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen, cleanup, within } from "@testing-library/react";
 
 import { AppointmentForm } from "../src/appointmentForm";
 import type { Service, Appointment } from "../src/appointmentForm";
@@ -70,5 +70,38 @@ describe('Appointment form', ()=>{
       assert.ok(option.tagName==='OPTION' && option.selected, `Expected option ${appointment.service} to be selected`)
     })
   })
+  describe("time slot table", () => {
+    it("renders a table for time slots with an id", () => {
+      render(<AppointmentForm appointment={blankAppointment} />);
+      const table = screen.getByLabelText('time slot table') as HTMLTableElement;
+
+      assert.ok(table, 'No table found with aria-label "time slot table"');
+    });
+    it("renders time slot headings", () => {
+      render(<AppointmentForm appointment={blankAppointment} today={new Date(2018, 11, 1)} />);
+      const table = screen.getByLabelText('time slot table') as HTMLTableElement;
+      const rowGroups = within(table).getAllByRole('rowgroup') as HTMLTableRowElement[];
+      const [theadRowGroup, tbodyRowGroup] = rowGroups
+      const timesOfDayHeadings = within(tbodyRowGroup).getAllByRole('columnheader') as HTMLTableRowElement[];
+      assert.deepEqual(timesOfDayHeadings[0].textContent, '09:00', `Expected column heading "09:00"`);
+      assert.deepEqual(timesOfDayHeadings[1].textContent, '09:30', `Expected column heading "09:30"`);
+      assert.deepEqual(timesOfDayHeadings[2].textContent, '10:00', `Expected column heading "10:00"`);
+    });
+    it("renders day of week headings", () => {
+      render(<AppointmentForm appointment={blankAppointment} today={new Date(2018, 11, 1)} />);
+      const table = screen.getByLabelText('time slot table') as HTMLTableElement;
+      const rowGroups = within(table).getAllByRole('rowgroup') as HTMLTableRowElement[];
+      const [theadRowGroup, tbodyRowGroup] = rowGroups
+      const row = within(theadRowGroup).getAllByRole('row');
+      const dates = within(theadRowGroup).getAllByRole('columnheader') as HTMLTableRowElement[];
+
+      assert.deepEqual(row[0].textContent, 'Sat 01Sun 02Mon 03Tue 04Wed 05Thu 06Fri 07', `Expected:"Sat 01Sun 02Mon 03Tue 04Wed 05Thu 06Fri 07"`);
+      assert.deepEqual(dates[0].textContent, '', `Expected empty heading column`);
+      assert.deepEqual(dates[1].textContent, 'Sat 01', `Expected column heading "Sat 01"`);
+      assert.deepEqual(dates[2].textContent, 'Sun 02', `Expected column heading "Sun 02"`);
+      assert.deepEqual(dates[3].textContent, 'Mon 03', `Expected column heading "Mon 03"`);
+      assert.deepEqual(dates[4].textContent, 'Tue 04', `Expected column heading "Tue 04"`);
+    });
+  });
 })
 
