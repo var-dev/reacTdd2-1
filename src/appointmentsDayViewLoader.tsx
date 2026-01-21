0c7149c2cafa1730c14dc44d8bb6b14216398148
user <user@localhost>
2026-01-20 23:03:47 -0700

    p197 useState

--- src/appointmentsDayViewLoader.tsx
+++ src/appointmentsDayViewLoader.tsx
@@ -1,3 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { AppointmentsDayView } from "./appointmentsDayView.js";
-export const AppointmentsDayViewLoader = () => <AppointmentsDayView appointments={[]}/>;
+
+export type AppointmentsDayViewLoaderProps = {today: Date}
+export const AppointmentsDayViewLoader = (
+  {
+    today
+  }: AppointmentsDayViewLoaderProps) => {
+  const [appointments, setAppointments] = useState([]);
+  useEffect(() => {
+    const from = today.setHours(0, 0, 0, 0);
+    const to = today.setHours(23, 59, 59, 999);
+    globalThis.fetch(
+        `/appointments/${from}-${to}`,
+        {
+          method: "GET",
+          credentials: "same-origin",
+          headers: {
+            "Content-Type": "application/json"
+          },
+        }
+      )
+      .then(response => response.json())
+      .then(appointments => {
+        setAppointments(appointments)
+      })
+      .catch((e) => {
+        // console.log(e)
+      })  
+  }, []);
+  return <AppointmentsDayView appointments={appointments}/>
+  }
--- test/appointmentsDayViewLoader.test.tsx
+++ test/appointmentsDayViewLoader.test.tsx
@@ -10,7 +10,7 @@ import userEvent from "@testing-library/user-event";
 
 import { AppointmentForm, serviceStylists, stylists } from "../src/appointmentForm.tsx";
 import type { Service, Appointment, AppointmentFormProps, AvailableTimeSlot } from "../src/appointmentForm.tsx";
-
+import type { AppointmentsDayViewLoaderProps } from "../src/appointmentsDayViewLoader.tsx";
 
 import quibble from 'quibble'
 
@@ -20,12 +20,14 @@ async function importSpyAppointmentsDayViewLoader() {
   await quibble.esm('../src/appointmentsDayView.tsx', {
     AppointmentsDayView: mockAppointmentsDayView
   })
-  const {AppointmentsDayView} = await import("../src/appointmentsDayView.tsx")
   const { AppointmentsDayViewLoader } = await import( "../src/appointmentsDayViewLoader.tsx");
   return {AppointmentsDayViewLoader, mockAppointmentsDayView}
 }
 
+const today = new Date(2018, 11, 1);
 const originalFetch = globalThis.fetch;
+const mockFetchOk = (...args: any[]) => Promise.resolve({ ok: true, json: ()=>Promise.resolve(args) });
+const mockFetchError = (...args: any[]) => Promise.resolve({ ok: false });
 
 beforeEach( async () => {
   quibble.reset()
@@ -37,23 +39,63 @@ afterEach(()=>{
   globalThis.fetch = originalFetch;
 })
 
-const testProps = {}
+const testProps: AppointmentsDayViewLoaderProps = {
+  today
+}
 
 describe('AppointmentsDayViewLoader',  ()=>{
+  const appointments: Appointment[] = [
+    { startsAt: today.setHours(9, 0, 0, 0), },
+    { startsAt: today.setHours(9, 30, 0, 0), },
+  ]
   it('renders AppointmentsDayView', async ()=>{
     const {AppointmentsDayViewLoader} = await importSpyAppointmentsDayViewLoader()
     render(<AppointmentsDayViewLoader {...testProps} />)
 
     assert.strictEqual(screen.getByTestId('appointmentsDayView').tagName, 'DIV')
   })
-  it("initially passes empty array of appointments toAppointmentsDayView", async () => {
+  it("initially passes empty array of appointments to AppointmentsDayView", async () => {
     const {AppointmentsDayViewLoader, mockAppointmentsDayView} = await importSpyAppointmentsDayViewLoader()
     render(<AppointmentsDayViewLoader {...testProps} />)
 
     assert.strictEqual(mockAppointmentsDayView.mock.calls.length, 1, `mockAppointmentsDayView should be called once`)
-    
+
     const expectedArgs = [{appointments: []}, undefined]
     const actualArgs = mockAppointmentsDayView.mock.calls[0].arguments
     assert.deepStrictEqual(actualArgs, expectedArgs, `mockAppointmentsDayView should be called with {appointments: []} but got ${JSON.stringify(actualArgs)}`)
+  })
+  it("fetches data when component is mounted", async () => {
+    const {AppointmentsDayViewLoader, } = await importSpyAppointmentsDayViewLoader()
+    const from = today.setHours(0, 0, 0, 0)
+    const to = today.setHours(23, 59, 59, 999);
+    const mockFetch = mock.method(globalThis,'fetch', mockFetchOk)
+    render(
+      <AppointmentsDayViewLoader today={today} />
+    );
+    assert.strictEqual(mockFetch.mock.calls.length, 1, `mockFetch is expected to be called 1 time, but was ${mockFetch.mock.calls.length}`)
+
+    const expectedArgs = [`/appointments/${from}-${to}`,{"method":"GET","credentials":"same-origin","headers":{"Content-Type":"application/json"}}]
+    const actualArgs = mockFetch.mock.calls[0].arguments
+    assert.deepStrictEqual(actualArgs, expectedArgs, `globalThis.fetch should be called with ${expectedArgs} but got ${JSON.stringify(actualArgs)}`)
+
+    const actualResult = await (await mockFetch.mock.calls[0].result)?.json()
+    assert.strictEqual(
+      JSON.stringify(expectedArgs), 
+      JSON.stringify(actualResult), 
+      `globalThis.fetch should be returning \n${JSON.stringify(expectedArgs)} but got \n${JSON.stringify(actualResult)}`)
+  });
+  it("passes fetched appointments to AppointmentsDayView", async () => {
+    const {AppointmentsDayViewLoader, mockAppointmentsDayView} = await importSpyAppointmentsDayViewLoader()
+    const from = today.setHours(0, 0, 0, 0)
+    const to = today.setHours(23, 59, 59, 999);
+    mock.method(globalThis, 'fetch', mockFetchOk)
+    render(<AppointmentsDayViewLoader today={today} />);
+    await screen.findByTestId('appointmentsDayView')
+
+    assert.strictEqual(mockAppointmentsDayView.mock.calls.length, 2, `mockAppointmentsDayView should be called twice`)
+
+    const expectedArgs = [{"appointments":[`/appointments/${from}-${to}`,{"method":"GET","credentials":"same-origin","headers":{"Content-Type":"application/json"}}]},undefined]
+    const actualArgs = mockAppointmentsDayView.mock.calls[1].arguments
+    assert.deepStrictEqual(actualArgs, expectedArgs, `mockAppointmentsDayView should be called with \n${JSON.stringify(expectedArgs)} but got \n${JSON.stringify(actualArgs)}`)
   })
 })
